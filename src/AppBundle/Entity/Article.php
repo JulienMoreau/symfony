4b747ee05dc43a2deb23593dc3b214f5c03fb03f
Julien Moreau <julien.moreau@adfab.fr>
2017-06-09 14:12:42 -0400

    Entity article and author

--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -11,6 +11,45 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Article {
 
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $title;
+
+    public function getId()
+    {
+        return $this->id;
+    }
 
 
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 }
